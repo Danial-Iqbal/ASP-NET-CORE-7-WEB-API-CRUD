76c3291c42bf5c6b0d91fb77eb8246dfd073ef53
DANIAL IQBAL <expertdanial@gmail.com>
2026-02-05 22:25:19 +0400

    Remove Bin Obj Files

--- src/CRUD.Model/obj/Debug/net7.0/CRUD.Model.AssemblyInfo.cs
+++ src/CRUD.Model/obj/Debug/net7.0/CRUD.Model.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUD.Model")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ba54103987ca0c576333e93699cc88ec32dadac")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUD.Model")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUD.Model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
